23b02bb03bd58da64fb23b6feb528a88e9d2863c
catoraa <1164726499@qq.com>
2023-10-04 11:02:27 +0800

    1

---
+++ 2023-408.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+const int MAXV=100;
+
+typedef struct{
+    int numVertices,numEdge;
+    char VerticesList[MAXV];
+    int Edge[MAXV][MAXV];
+}MGraph;
+
+int printVertices(MGraph G){
+       return 0;
+}
+
+
+int main(){
+    return 0;
+}
