438d8de743c5446248db8b1ee0b7e5e4bd95b56c
catoraa <1164726499@qq.com>
2023-10-04 12:08:16 +0800

    1

--- 2023-408.cpp
+++ 2023-408.cpp
@@ -10,7 +10,19 @@ typedef struct{
 }MGraph;
 
 int printVertices(MGraph G){
-       return 0;
+    int K=0;
+    for(int i=0;i<G.numVertices;i++){
+        int in=0,out=0;//入度和出度的计数置零
+        for(int j=0;j<G.numEdge;j++){
+            out=out+G.Edge[i][j];//出度为i行所有元素之和
+            in=in+G.Edge[j][i];//入度为i列所有元素之和
+        }
+        if(out>in){
+            printf("%c",G.VerticesList[i]);
+            K++;
+        }
+    }
+       return K;
 }
 
 
